bf5db0eda602681e09c5f2a861672b9e9a5b45a1
Leon Nascimento <l.nasc@live.com>
2018-02-06 00:00:27 -0200

    [MANUT] Removi os arquivos cache que seriam enviados

--- Views/cache/footer.91ef5fef9d1d2d6870e61a06b0aeb283.rtpl.php
+++
@@ -1,8 +0,0 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?></div>
-      <footer class= "pagebottom">
-        <center>
-          PACO | Leon Nascimento - 2016 - 2018
-        </center>
-      </footer>
-  </BODY>
-</HTML>
--- Views/cache/meta.91ef5fef9d1d2d6870e61a06b0aeb283.rtpl.php
+++
@@ -1,8 +0,0 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><head>
-  <title><?php echo htmlspecialchars( $title, ENT_COMPAT, 'UTF-8', FALSE ); ?></title>
-  <!-- CSS e similares -->
-  <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" />
-  <link rel="stylesheet" href="Views/CSS/styles.css"/>
-  <!-- Scripts -->
-  <script src="script.js"></script>
-</head>
