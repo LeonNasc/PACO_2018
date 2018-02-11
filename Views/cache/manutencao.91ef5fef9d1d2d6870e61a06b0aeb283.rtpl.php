dd164410aa56dba29e398b4cd0222c8c36a65204
Leon de França Nascimento <l.nasc@live.com>
2018-02-11 04:33:10 +0000

    [Limpeza]Removi arquivos cache

--- Views/cache/manutencao.91ef5fef9d1d2d6870e61a06b0aeb283.rtpl.php
+++
@@ -1,4 +0,0 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><center>
-  Em Manutenção
-  <img class="thumbnail" src="https://media1.tenor.com/images/fdb68e17093a027ec5f87a2f666acf3f/tenor.gif?itemid=4819844">
-</center>
