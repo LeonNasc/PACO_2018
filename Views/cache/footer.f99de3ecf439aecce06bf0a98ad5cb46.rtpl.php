58bf2084d9795e514c69c25864a59cb7d571dd9b
Leon de França Nascimento <l.nasc@live.com>
2018-02-13 19:56:57 +0000

    [Limpeza] Removendo cache

--- Views/cache/footer.f99de3ecf439aecce06bf0a98ad5cb46.rtpl.php
+++
@@ -1,8 +0,0 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?></div> <!--End maindiv -->
-</div>
-<footer class= "pagebottom">
-  <center>
-    PACO | Leon Nascimento - 2016 - 2018
-  </center>
-</footer>
-
