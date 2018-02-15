b7d3bc8e242ced4cecfd3e50feee814b0cadea33
Leon Nascimento <l.nasc@live.com>
2018-02-14 22:08:13 -0200

    [DEV] Montando front-end para landing e login

--- Controller/patients.php
+++ Controller/patients.php
@@ -2,10 +2,11 @@
 
 if ($_SERVER['method'] == GET){
   // Deve devolver um JSON com a lista de pacientes do usuário logado
-
+  echo "Hello world!";
 }
 else if ($_SERVER['method'] == POST){
   //Deve lidar com as funções ADD, DELETE e UPDATE paciente
+  echo "Hello world!2";
 }
 else {
   throw new Exception("Metodo de acesso inválido");
