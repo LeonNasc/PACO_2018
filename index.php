d547bfbefbf7fd74384c9767ea9226c8b43a143a
Leon Nascimento <l.nasc@live.com>
2018-03-25 21:53:12 -0300

    [conflict]Pull nas mudanças feitas no c9

--- index.php
+++ index.php
@@ -8,4 +8,31 @@ if(isset($_SESSION['active_user_id'])){
 else
   Helper::show_landing();
 
+
+/* TODO: Implementar ROTAS
+//Quebra a URI em duas partes
+$request_uri = explode('?', $_SERVER['REQUEST_URI'], 2);
+
+// Rotas
+switch ($request_uri[0]) {
+  // Home
+  case '/':
+      if(isset($_SESSION['active_user_id'])){
+        $_SESSION['patient_list'] = json_decode(Patient::get_patient_list($_SESSION['active_user_id']['id']),true);
+        Helper::make_template('dashboard',array('patients'=>$_SESSION['patient_list']));
+        }
+      else
+        Helper::show_landing();
+      break;
+  // Acompanhamento
+  case '/acompanhar':
+      require '../views/about.php';
+      break;
+  // Everything else
+  default:
+      header('HTTP/1.0 404 Not Found');
+      require 'Views/templates/404.html';
+      break;
+}
+*/
 ?>
